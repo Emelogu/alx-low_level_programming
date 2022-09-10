e253aebe5319cdab182c2b2cff782e2f65166d0f
Alexander Emelogu <78867434+Emelogu@users.noreply.github.com>
2022-09-11 00:12:02 +0100

    Second commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,14 +3,15 @@
 #include <stdio.h>
 
 /**
- * main - Prints the last digit of a randomly generated number
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
+ 
 int main(void)
 {
 	int n;
-	srand(time(0));
+	srand(time(0)); 
 	n = rand() - RAND_MAX / 2;
 	if ((n % 10) > 5)
 	{

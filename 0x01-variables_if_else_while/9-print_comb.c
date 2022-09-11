2761828f3ae1643a7202fd72a5c9a29b6083a3f6
Emelogu <emelogualex20@gmail.com>
2022-09-11 23:51:08 +0100

    Second commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{	int c;
+	for (c = 48; c <= 57; c++)
+	{
+		putchar(c);
+		if (c != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}

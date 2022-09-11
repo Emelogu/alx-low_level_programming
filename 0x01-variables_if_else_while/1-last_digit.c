d19b5aa1a5159109f781e043cfaf5ea78b86651d
Alexander Emelogu <78867434+Emelogu@users.noreply.github.com>
2022-09-11 22:45:18 +0100

    Second commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -7,15 +7,14 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{       int n;
-	int m;
+{ int n;
+  int m;
 	
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	m = n % 10;
-	
-	if (m > 5)
-	{
+  srand(time(0));
+  n = rand() - RAND_MAX / 2;
+  m = n % 10;	
+  if (m > 5)
+        {
 		printf("Last digit of %d is %d and is greater than 5\n", n, m);
 	}
 	else if (m < 6 && m != 0)

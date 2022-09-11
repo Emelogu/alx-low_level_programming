eb2ebf341650c895d7034e599c050f63a4f14ed1
Alexander Emelogu <78867434+Emelogu@users.noreply.github.com>
2022-09-11 21:09:42 +0100

    Second commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,6 +9,7 @@
 int main(void)
 {       int n;
 	int m;
+	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	m = n % 10;
@@ -24,5 +25,6 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, m);
 	}
+	
 	return (0);
 }

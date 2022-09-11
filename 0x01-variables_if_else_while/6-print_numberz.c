1f0f68a188a0a54aa2de2bb3fd9f1f69cfff8451
Alexander Emelogu <78867434+Emelogu@users.noreply.github.com>
2022-09-12 00:43:09 +0100

    Second commit

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,10 +5,10 @@
  * Return: Always 0 (Succcess)
  */
 int main(void)
-{	int i;
-	for (i = 0; i < 10; i++)
+{	int 2;
+	for (2 = 0; 2 < 10; 2++)
 	{
-		putchar(i);
+		putchar(2);
 	}
 	putchar('\n');
 	return (0);

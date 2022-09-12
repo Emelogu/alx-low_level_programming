460775425d3f2118ac43baeb1cfded7b99a76dcb
Emelogu <emelogualex20@gmail.com>
2022-09-12 13:39:41 +0100

    Second commit

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - print possibe combinations
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n1 = 0;
+	int n2;
+	while (n1 <= 99)
+	{
+		n2 = n1;
+		while (n2 <= 99)
+		{
+			if (n2 != n1)
+			{
+				putchar((n1 / 10) + 48);
+				putchar((n1 % 10) + 48);
+				putchar(' ');
+				putchar((n2 / 10) + 48);
+				putchar((n2 % 10) + 48);
+
+				if (n1 != 98 || n2 != 98)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			++n2;
+		}
+		++n1;
+	}
+	putchar('\n');
+	return (0);
+}

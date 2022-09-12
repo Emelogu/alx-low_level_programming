e49100dc7e08348fa4faa4b845ad4f11991fe0ea
Alexander Emelogu <78867434+Emelogu@users.noreply.github.com>
2022-09-12 20:43:54 +0100

    Second commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,10 +6,10 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{	int n1 = 0;
-	int n2 = 0;
+{	int n1 = 0, n2;
 	while (n1 <= 99)
-	{	while (n2 <= 99)
+	{	n2 = n1;
+		while (n2 <= 99)
 		{
 			if (n2 != n1)
 			{	putchar((n1 / 10) + 48);

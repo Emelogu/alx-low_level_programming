761fd548ad111c8e11ddd42c25ef0fbbd8efc226
Alexander Emelogu <78867434+Emelogu@users.noreply.github.com>
2022-09-12 20:17:04 +0100

    Second commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - print possibe combinations
  *
@@ -7,12 +8,11 @@
 int main(void)
 {	int n1 = 0, n2;
  	while (n1 <= 99)
-	{	
-		n2 = n1;
+	{	n2 = n1;
 		while (n2 <= 99)
-		{
+		{	
 			if (n2 != n1)
-			{
+			{	
 				putchar((n1 / 10) + 48);
 				putchar((n1 % 10) + 48);
 				putchar(' ');

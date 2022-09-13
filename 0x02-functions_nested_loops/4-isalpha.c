12c40b6380b41307490e9d5bc43a7215eb7099a5
Alexander Emelogu <78867434+Emelogu@users.noreply.github.com>
2022-09-13 23:08:13 +0100

    Third commit

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _isalpha - function that checks for alphabetic character
- *
+ * @c: is the int that will be for the argument of the function
  * Return: Aways 0
  */
 int _isalpha(int c)

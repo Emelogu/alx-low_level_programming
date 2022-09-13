67ab42078ce9d9036bfd581a905cdfa483c9396f
Alexander Emelogu <78867434+Emelogu@users.noreply.github.com>
2022-09-13 22:22:15 +0100

    Third commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
  * print alphabet - prints lowercase letters
- * @c: is the int for function argument
+ * @c: is the int that will be for the argument of the function
  * Return: Always 0 (Success)
  */
 int _islower(int c)
+
 {	if (c >= 'a' && c <= 'z')
 	{
 		return (1);

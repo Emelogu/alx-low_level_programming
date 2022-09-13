211ac6d24c275ace1f4bcb7ae2048c4256d74fb5
Alexander Emelogu <78867434+Emelogu@users.noreply.github.com>
2022-09-13 22:28:53 +0100

    Third commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,7 +4,7 @@
  * @c: is the int that will be for the argument of the function
  * Return: Always 0 (Success)
  */
-int _islower(void)
+int _islower(int c)
 {	if (c >= 'a' && c <= 'z')
 	{
 		return (1);

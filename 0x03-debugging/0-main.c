bbf624c1dc850ac994ce955d2099a9f0ac3cfc9e
Alexander Emelogu <78867434+Emelogu@users.noreply.github.com>
2022-09-15 20:56:39 +0100

    Fourth commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,28 +1,16 @@
 #include "main.h"
 
 /**
-<<<<<<< HEAD
- * main - tests function for positive or negative
- *
- * Return: 0
-=======
  * main - tests function that prints if integer is positive or negative
- * Return: Always 0 
->>>>>>> f277ca9318365bfb4a037b6ba89865b21265281f
+ * Return: 0
  */
 
 int main(void)
 {
-	int i;
-<<<<<<< HEAD
+int i;
 
-	i = 0;
-	positive_or_negative(i);
+i = 0;
+positive_or_negative(i);
 
-	return(0);
-=======
-	i = 0;
-	positive_or_negative(i);
- 	return (0);
->>>>>>> f277ca9318365bfb4a037b6ba89865b21265281f
+return (0);
 }
